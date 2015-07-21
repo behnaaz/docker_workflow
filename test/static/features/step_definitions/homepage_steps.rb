92616933c737018bca491da138da98a864935f4b
Daniël van Gils <daniel@steam.nl>
2015-07-21 11:30:20 +0200

    update example

--- test/static/features/step_definitions/homepage_steps.rb
+++ test/static/features/step_definitions/homepage_steps.rb
@@ -1,5 +1,5 @@
 Gegeven(/^dat er een aantal quotes zijn over Docker$/) do
-  @player = create :player
+  @quote = create :quote
 end
 
 Stel(/^dat ik de fantastische homepage bezoek$/) do
@@ -7,7 +7,7 @@ Stel(/^dat ik de fantastische homepage bezoek$/) do
 end
 
 Dan(/^zie ik een overzicht vanb alle quotes$/) do
-  expect(page).to have_content @player.name
+  expect(page).to have_content @quote.text
 end
 
 Dan(/^maken wij voor de test een screenshot met de naam "([^"]*)"$/) do |filename|
